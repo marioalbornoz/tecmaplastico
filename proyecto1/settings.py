14cf17af9b7a6f1dfaa6f02a82e68ecddfb7b01e
marioalbornoz <mario.albornozc@utem.cl>
2019-12-19 01:06:17 -0300

    se agrega navbar

--- proyecto1/settings.py
+++ proyecto1/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 LOGIN_REDIRECT_URL = reverse_lazy('produccion_app:produccion')
 

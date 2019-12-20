c6035822f95430bdcbe44a2cb296693740db2789
marioalbornoz <mario.albornozc@utem.cl>
2019-12-20 18:37:22 -0300

    se agrega bootstrap

--- proyecto1/settings.py
+++ proyecto1/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 LOGIN_REDIRECT_URL = reverse_lazy('produccion_app:produccion')

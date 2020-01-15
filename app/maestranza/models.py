b8224dad63334affbe59ae42038eff45acfc8d7d
marioalbornoz <mario.albornozc@utem.cl>
2020-01-15 00:12:48 -0300

    Se agrega prioridad y estado al modelo proyecto

--- app/maestranza/models.py
+++ app/maestranza/models.py
@@ -3,13 +3,22 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-
-
+ESTADOS = (
+    (0, 'Asignado'),
+    (1, 'trabajando'),
+    (2, 'terminado'),
+)
+PRIORIDAD = (
+    (0, 'Baja'),
+    (1, 'Media'),
+    (2, 'Alta'),
+)
 class Persona(models.Model):
     nombre = models.CharField(max_length=50)
     apellidos = models.CharField(max_length=50)
     telefono = models.CharField(max_length=12)
     correo = models.EmailField()
+    foto = models.ImageField(upload_to='profile', null=True, blank=True)
     usuario = models.OneToOneField(User, on_delete=models.CASCADE, default=True)
 
 
@@ -24,6 +33,7 @@ class Proyecto(models.Model):
     #persona = models.ForeignKey(
     #    Persona, null=True, blank=True, on_delete=models.CASCADE)
     persona = models.ManyToManyField(Persona, blank=True)
-
+    estado = models.IntegerField(choices=ESTADOS, default=0)
+    prioridad = models.IntegerField(choices=PRIORIDAD, default=0)
     def __str__(self):
         return '{} {}'.format(self.nombre_proyecto, self.empresa)
